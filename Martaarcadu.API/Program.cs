0fd5e6310610d53afe4f5b526a36479684762511
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-01 14:52:58 +0600

    SubscriptionsPlan related service injected.

--- Martaarcadu.API/Program.cs
+++ Martaarcadu.API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
 
 
 builder.Services.AddScoped<IServiceTypeService,ServiceTypeService>();
-
+builder.Services.AddScoped<ISubscriptionsPlanService,SubscriptionsPlanService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
