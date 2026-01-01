d80fc60d416cf54ef3d56a8c3e7ec6074b1964a5
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-01 12:26:18 +0600

    DTO for APIResponse

---
+++ Martaarcadu.Application/DTOs/APIResponse/APIResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Application.DTOs.APIResponse
+{
+    public class APIResponseDto
+    {
+        public object? ResponseObject { get; set; } = null;
+        public bool IsSuccess { get; set; } = false;
+        public string Message { get; set;  }=string.Empty;
+    }
+}
