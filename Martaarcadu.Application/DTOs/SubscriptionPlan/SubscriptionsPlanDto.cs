94132bc57630a0dd02aafed92225b893a4549de6
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-01 14:51:49 +0600

    DTOs for SubscriptionPlans added.

---
+++ Martaarcadu.Application/DTOs/SubscriptionPlan/SubscriptionsPlanDto.cs
@@ -0,0 +1,25 @@
+﻿using Martaarcadu.Domain.Entities.SubscriptionPlan;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Martaarcadu.Domain.Enums.Enums;
+
+namespace Martaarcadu.Application.DTOs.SubscriptionPlan
+{
+    public class SubscriptionsPlanDto
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; } = 0;
+
+        public DurationType Duration { get; set; } = DurationType.Free;
+        public ICollection<string>? Benefits { get; set; }
+    }
+}
---
+++ Martaarcadu.Application/DTOs/SubscriptionPlan/UpdatePlanDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Martaarcadu.Domain.Enums.Enums;
+
+namespace Martaarcadu.Application.DTOs.SubscriptionPlan
+{
+    public class UpdatePlanDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; } = 0;
+
+        public DurationType Duration { get; set; } = DurationType.Free;
+        public ICollection<Guid>? BenefitIds { get; set; }
+    }
+}
