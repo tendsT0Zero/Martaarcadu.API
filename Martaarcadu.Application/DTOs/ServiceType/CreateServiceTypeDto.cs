270c7ab85af4f70a0c495c17030ab06223615106
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-01 12:24:38 +0600

    ServiceType related DTOs are added

---
+++ Martaarcadu.Application/DTOs/ServiceType/CreateServiceTypeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Application.DTOs.ServiceType
+{
+    public class CreateServiceTypeDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        
+    }
+}
---
+++ Martaarcadu.Application/DTOs/ServiceType/ServiceTypeResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Application.DTOs.ServiceType
+{
+    public class ServiceTypeResponseDto
+    {
+        public Guid ServiceId { get; set; }
+        public string ServiceName { get; set; } = string.Empty;
+    }
+}
