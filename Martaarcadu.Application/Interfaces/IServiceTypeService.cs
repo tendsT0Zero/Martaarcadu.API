54e73fd43496e27d4162e4fb72f5113dae0a4885
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-01 12:24:08 +0600

    Service Interfaces are added for CRUD operation on ServiceType

---
+++ Martaarcadu.Application/Interfaces/IServiceTypeService.cs
@@ -0,0 +1,19 @@
+﻿using Martaarcadu.Application.DTOs.APIResponse;
+using Martaarcadu.Application.DTOs.ServiceType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Application.Interfaces
+{
+    public interface IServiceTypeService
+    {
+        Task<APIResponseDto> GetAllAsync();
+        Task<APIResponseDto> GetByIdAsync(Guid id);
+        Task<APIResponseDto> CreateAsync(CreateServiceTypeDto dto);
+        Task<APIResponseDto> UpdateAsync(Guid id, UpdateServiceTypeDto dto);
+        Task<APIResponseDto> DeleteAsync(Guid id);
+    }
+}
