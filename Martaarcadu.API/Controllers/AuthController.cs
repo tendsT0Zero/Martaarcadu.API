6e6de1b682900b4c1b05bb4b42659437aac761cc
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-05 10:53:14 +0600

    auth related api endpoints are establshed.

---
+++ Martaarcadu.API/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Martaarcadu.Application.DTOs.Auth;
+using Martaarcadu.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Martaarcadu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _authService.RegisterAsync(dto);
+
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+
+        [HttpPost("verify-email")]
+        public async Task<IActionResult> VerifyEmail([FromBody] VerifyEmailDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _authService.VerifyEmailAsync(dto);
+
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _authService.LoginAsync(dto);
+            return result.IsSuccess ? Ok(result) : Unauthorized(result);
+        }
+
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _authService.ForgotPasswordAsync(dto);
+
+            return Ok(result);
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _authService.ResetPasswordAsync(dto);
+
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+    }
+}
