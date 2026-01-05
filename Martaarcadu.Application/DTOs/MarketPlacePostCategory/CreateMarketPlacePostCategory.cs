38fabf3fe408889e64bbc136d20ae6bb8fd4d5e2
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-05 16:07:36 +0600

    Dtos are added for crud operations

---
+++ Martaarcadu.Application/DTOs/MarketPlacePostCategory/CreateMarketPlacePostCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Martaarcadu.Domain.Enums.Enums;
+
+namespace Martaarcadu.Application.DTOs.MarketPlacePostCategory
+{
+    public class CreateMarketPlacePostCategory
+    {
+        public string Name { get; set; }
+    }
+}
