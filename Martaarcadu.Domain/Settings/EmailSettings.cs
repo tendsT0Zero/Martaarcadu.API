a10717471dc1fa4f14186dd1a960212da1a1861f
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-05 10:52:47 +0600

    email settings attributes are added in a model class

---
+++ Martaarcadu.Domain/Settings/EmailSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Domain.Settings
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public string SenderName { get; set; } = string.Empty;
+        public string SenderEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool EnableSsl { get; set; }
+    }
+}
