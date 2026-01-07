03a21bace9e8a20dc69751a6d672f01bf2f0b81c
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-07 10:05:09 +0600

    Entities added for 1-to-1 chatting on a post

---
+++ Martaarcadu.Domain/Entities/Chat/ChatMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Domain.Entities.Chat
+{
+    public class ChatMessage
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid ConversationId { get; set; }
+        [ForeignKey("ConversationId")]
+        public Conversation Conversation { get; set; }
+
+        public Guid SenderId { get; set; }
+        [ForeignKey("SenderId")]
+        public ApplicationUser.ApplicationUser Sender { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+        public bool IsRead { get; set; }
+    }
+}
---
+++ Martaarcadu.Domain/Entities/Chat/Conversation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Domain.Entities.Chat
+{
+    public class Conversation
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime LastMessageAt { get; set; } = DateTime.UtcNow;
+
+        // Relation to the specific Post being discussed
+        public Guid PostId { get; set; }
+        [ForeignKey("PostId")]
+        public MarketPlacePost.MarketPlacePost Post { get; set; }
+
+        // The owner of the post
+        public Guid PostOwnerId { get; set; }
+        [ForeignKey("PostOwnerId")]
+        public ApplicationUser.ApplicationUser PostOwner { get; set; }
+
+        // The user interested in the post (The Buyer/Inquirer)
+        public Guid ParticipantId { get; set; }
+        [ForeignKey("ParticipantId")]
+        public ApplicationUser.ApplicationUser Participant { get; set; }
+
+        public ICollection<ChatMessage> Messages { get; set; }
+    }
+}
