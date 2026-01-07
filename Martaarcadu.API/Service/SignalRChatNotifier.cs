edc674b19394b02ffc7a0cb93fb5bbb29c960b34
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-07 10:56:41 +0600

    Realtime Notification services added

---
+++ Martaarcadu.API/Service/SignalRChatNotifier.cs
@@ -0,0 +1,24 @@
+﻿using Martaarcadu.Application.DTOs.Chat;
+using Martaarcadu.Application.Interfaces;
+using Martaarcadu.Infrastructure.SignalR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Martaarcadu.API.Service
+{
+    public class SignalRChatNotifier : IChatNotificationService
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public SignalRChatNotifier(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task NotifyNewMessageAsync(string conversationId, MessageResponseDto message)
+        {
+            
+            await _hubContext.Clients.Group(conversationId)
+                .SendAsync("ReceiveMessage", message);
+        }
+    }
+}
---
+++ Martaarcadu.Application/Interfaces/IChatNotificationService.cs
@@ -0,0 +1,14 @@
+﻿using Martaarcadu.Application.DTOs.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Application.Interfaces
+{
+    public interface IChatNotificationService
+    {
+        Task NotifyNewMessageAsync(string conversationId, MessageResponseDto message);
+    }
+}
