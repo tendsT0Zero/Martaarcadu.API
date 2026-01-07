a9a42460c0916d90f57dfa4a07e1a6771f60f12d
tendsT0Zero <nayeemofficial80@gmail.com>
2026-01-07 10:38:00 +0600

    Create chathub using signalR

---
+++ Martaarcadu.Infrastructure/SignalR/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Martaarcadu.Infrastructure.SignalR
+{
+    public class ChatHub:Hub
+    {
+        public async Task JoinConversation(string conversationId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conversationId);
+        }
+
+        public async Task LeaveConversation(string conversationId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, conversationId);
+        }
+    }
+}
